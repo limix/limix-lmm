6a7864ba654d84cdd79c438647dc677f0a5eca8f
Danilo Horta <danilo.horta@gmail.com>
2018-11-01 10:19:29 +0000

    test it

--- limix_lmm/__init__.py
+++ limix_lmm/__init__.py
@@ -10,7 +10,8 @@ Flexible Linear mixed model Toolbox for Genome-wide association studies
 from __future__ import absolute_import as _absolute_import
 from . import plot
 from .lmm_core import LMM
+from ._testit import test
 
 __version__ = "0.0.1"
 
-__all__ = ["LMM", "plot", "__version__"]
+__all__ = ["LMM", "plot", "__version__", "test"]
